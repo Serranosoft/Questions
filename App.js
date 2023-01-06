8aad9124f23c4cbdb2a32694762fdcc94da5fe0b
Manuel Scholz <manuel.serrano943@gmail.com>
2023-01-06 14:50:20 +0100

    Feat: Eliminar appLoading y usar SplashScreen ya que AppLoading esta deprecated

--- App.js
+++ App.js
@@ -2,16 +2,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/Home';
 import * as Font from "expo-font";
-import Apploading from "expo-app-loading";
 import { useState, useEffect } from 'react';
 import Questions from './screens/Questions';
 import 'react-native-url-polyfill/auto';
 import Names from './screens/Names';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
 
     const Stack = createNativeStackNavigator();
-    const [fontsloaded, setFontsLoaded] = useState(false);
+    const [appIsReady, setAppIsReady] = useState(false);
 
     const getFonts = () => Font.loadAsync({
         heading: require("./assets/fonts/Source_Code_Pro/static/SourceCodePro-SemiBold.ttf"),
@@ -19,11 +21,37 @@ export default function App() {
         text: require("./assets/fonts/Nunito_Sans/NunitoSans-Regular.ttf")
     });
 
-    if (fontsloaded) {
+    useEffect(() => {
+        async function prepare() {
+            try {
+                // Pre-load fonts, make any API calls you need to do here
+                getFonts();
+            } catch (e) {
+                console.warn(e);
+            } finally {
+                // Tell the application to render
+                setAppIsReady(true);
+            }
+        }
+
+        prepare();
+    }, []);
+
+    const hideSplashScreen = async () => {
+        await SplashScreen.hideAsync();
+    }
+
+    useEffect(() => {
+        if (appIsReady) {
+            hideSplashScreen();
+        }
+    }, [appIsReady])
+
+    if (appIsReady) {
         return (
-            
+
             <NavigationContainer>
-                <Stack.Navigator>
+                <Stack.Navigator initialRouteName='Home'>
                     <Stack.Screen
                         name="Home"
                         component={Home}
@@ -42,15 +70,6 @@ export default function App() {
                 </Stack.Navigator>
             </NavigationContainer>
         );
-    } else {
-        return (
-            <Apploading
-                startAsync={getFonts}
-                onFinish={() => {
-                    setFontsLoaded(true);
-                }}
-                onError={console.warn}
-            />
-        );
     }
+
 }
