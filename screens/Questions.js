7ad1f7300a42e3f910347d6fcf416c936e51a6bc
Manuel Scholz <manuel.serrano943@gmail.com>
2023-02-17 10:31:25 +0100

    Fix: Mostrar rating modal si nunca ha establecido un asyncStorage como condición para que se vuelva a mostrar

--- screens/Questions.js
+++ screens/Questions.js
@@ -113,13 +113,16 @@ export default function Questions({ route }) {
 
         const dateStorage = await AsyncStorage.getItem("@launch-app-review");
         if (dateStorage) {
+            // Si existe asyncStorage, entonces comprobar si cumple la condición y entonces mostrar el rating modal.
             if (new Date().getTime() >= parseInt(JSON.parse(dateStorage))) {
                 showRatingModal();
             }
         } else {
+            // Si no existe asyncStorage, entonces implementarlo y mostrar el rating modal.
             let date = new Date();
             date.setDate(new Date().getDate() + 10);
             await AsyncStorage.setItem("@launch-app-review", JSON.stringify(date.getTime()));
+            showRatingModal();
         }
 
     }
