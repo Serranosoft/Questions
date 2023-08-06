a70e292a6e12cc3660da025cf8432b6f496965f8
Manuel Scholz <manuel.serrano943@gmail.com>
2023-08-06 21:14:17 +0200

    Fix: Indicar a supabase que las sesiones no se deben persistir

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,4 +4,4 @@ import 'react-native-url-polyfill/auto';
 const supabaseUrl = "https://ukgelqpfnlytdjdnyrcd.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrZ2VscXBmbmx5dGRqZG55cmNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzI1MDU1MTksImV4cCI6MTk4ODA4MTUxOX0.k4rVa2SMG2LIpB3Siyb6EjnzpHChGAih7kOBMWomdm0";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {auth: { persistSession: false }})
